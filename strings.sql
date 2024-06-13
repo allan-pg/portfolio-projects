67e07e565081859d8f6fdf66cf4eccbd0aff7a06
Allan Gathigia <62595869+allan-pg@users.noreply.github.com>
2024-06-14 00:24:40 +0300

    Add files via upload

---
+++ strings.sql
@@ -0,0 +1,7 @@
+---------------------------------STRINGS in MYSQL ------------------------------------
+-- These comprises some of string functions in mysql that enable us to work with strings
+
+use sales;
+SELECT * from customers;
+
+-- concatenate ------------------
